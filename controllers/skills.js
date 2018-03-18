2ab879df5c8007b5282022b1ce88de187800ea03
Natalia Petkevich <petkevich.natalli@gmail.com>
2018-03-18 21:46:10 +0300

    up skills

--- controllers/skills.js
+++ controllers/skills.js
@@ -17,23 +17,15 @@ module.exports = async (ctx) => {
       .push({ number: years, text: 'Лет на сцене в качестве скрипача' })
       .write();
   }
-  const arrOfNumbers = [];
-  Object.keys(ctx.request.body).map((key) => {
-    let value = ctx.request.body[key];
-    arrOfNumbers.push(value);
-  });
-  db._.mixin({
-    upNumber: function (array) {
-      for (let i = 0; i < array.length; i++) {
-        array[i] = arrOfNumbers[i];
-      }
-      return array;
-    }
-  });
-  await db.get('skills')
-    .map('number')
-    .upNumber()
+  if (!db.get('skills').value()) {
+    db.defaults({ skills: [] }).write();
+  }
+  db.get('skills').remove().write();
+  db.get('skills')
+    .push({ number: age, text: 'Возраст начала занятий на скрипке' })
+    .push({ number: concerts, text: 'Концертов отыграл' })
+    .push({ number: cities, text: 'Максимальное число городов в туре' })
+    .push({ number: years, text: 'Лет на сцене в качестве скрипача' })
     .write();
-  console.log(db.get('skills').value());
   ctx.render('pages/admin', { msgskill: 'Данные успешно изменены' });
 };
